13099ce9efa312979a1e90cfbc4f624da30b91ed
EC2 Default User <ginyofuel@gmail.com>
2022-08-07 08:26:58 +0000

    投稿一覧作成終了

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-
   root 'home#top'
+  get 'top', to: 'home#top'
   get 'about', to: 'home#about'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'posts/index', to: 'posts#index'
 end
