26670ea84c023e0505cf82b6731a7b91c31b1276
EC2 Default User <ginyofuel@gmail.com>
2022-08-08 23:04:51 +0000

    validation実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,46 @@
 class PostsController < ApplicationController
-  
+
   def new
+    @post = Post.new
   end
-  
+
   def create
-    
-   @post = Post.new(content: params[:id])
-   @post.save
-   redirect_to posts_index_url
+    @post = Post.new(content: params[:content])
+    if @post.save
+     flash[:notice] = "投稿しました!"
+     redirect_to posts_index_url
+    else
+      render :new
+    end
   end
-  
+
   def index
     @posts = Post.all.order(created_at: :desc)
   end
-  
+
   def show
     @post = Post.find(params[:id])
+  end
+  
+  def edit
+    @post = Post.find(params[:id])
+  end
+  
+  def update
+    @post = Post.find(params[:id])
+    @post.content = params[:content]
+    if @post.save
+    flash[:notice] = "投稿を編集しました"
+      redirect_to posts_index_url
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:notice] = "投稿を削除しました!"
+    redirect_to posts_index_url
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
   get 'posts/new', to: 'posts#new'
   get 'posts/index', to: 'posts#index'
   get 'posts/:id', to: 'posts#show',as: :post
+  get 'posts/:id/edit', to: 'posts#edit',as: :edit_post
+  post 'posts/:id/update', to: 'posts#update',as: :update_post
+  post'posts/:id/destroy', to: 'posts#destroy',as: :destroy_post
 end
