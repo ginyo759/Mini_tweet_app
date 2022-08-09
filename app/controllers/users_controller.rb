c5f9dca31183f0f25853a455c3371fd8d74a87a4
EC2 Default User <ginyofuel@gmail.com>
2022-08-09 04:18:42 +0000

    ユーザー登録

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,41 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def new
+   @user = User.new
+  end
+  
+  def create
+    @user = User.new(name: params[:name], email: params[:email])
+    if @user.save
+       flash[:notice] = "ユーザー登録が完了しました"
+       redirect_to user_url @user
+    else
+       render :new
+    end
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.name = params[:name]
+    @user.email = params[:email]
+    if @user.save
+      flash[:notice] = "ユーザー情報を編集しました。"
+      redirect_to user_url @user
+    else
+      render :edit
+    end
+  end
+  
+end
+
